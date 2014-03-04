ad37889709ce9711d3ad80b68171c3f97c762296
Kai Wirtz <kai.wirtz@hzg.de>
2014-03-04 09:52:56 +0100

    doxygen describepar added by parser

--- src/models/hzg/maecs/maecs.F90
+++ src/models/hzg/maecs/maecs.F90
@@ -100,6 +100,25 @@ integer,                  intent(in)            :: configunit
 ! !LOCAL VARIABLES:
 integer    :: namlst=19
 !!------- Initial values of model maecs ------- 
+!> \describepar{nutN_initial , \mathrm{DIN} , Dissolved Inorganic Nitrogen DIN, 30.0 mmol-N/m**3}
+!> \describepar{nutP_initial , \mathrm{Phy}_\mathrm{P} , Dissolved Inorganic Phosphorus DIP, 1.2 mmol-P/m**3}
+!> \describepar{nutS_initial , \mathrm{Phy}_\mathrm{Si} , Dissolved Inorganic Silicon Si, 20. mmol-Si/m**3}
+!> \describepar{phyC_initial , \mathrm{Phy}_\mathrm{C} , Phytplankton Carbon, 10 mmol-C/m**3}
+!> \describepar{phyN_initial ,  , Phytplankton Nitrogen, 0.8 mmol-N/m**3}
+!> \describepar{phyP_initial ,  , Phytplankton Phosphorus, 0.08 mmol-P/m**3}
+!> \describepar{phyS_initial ,  , Phytplankton Silicon, 0.8 mmol-Si/m**3}
+!> \describepar{zooC_initial ,  , Zooplankton Carbon, 0.1 mmol-C/m**3}
+!> \describepar{detC_initial ,  , Detritus Carbon, 1 mmol-C/m**3}
+!> \describepar{detN_initial ,  , Detritus Nitrogen, 0.1 mmol-N/m**3}
+!> \describepar{detP_initial ,  , Detritus Phosphorus, 0.01 mmol-P/m**3}
+!> \describepar{detS_initial ,  , Detritus Silicon, 0.1 mmol-Si/m**3}
+!> \describepar{domC_initial ,  , Dissolved Organic Carbon, 0.1 mmol-C/m**3}
+!> \describepar{domN_initial ,  , Dissolved Organic Nitrogen, 0.01 mmol-N/m**3}
+!> \describepar{domP_initial ,  , Dissolved Organic Phosphorus, 0.001 mmol-P/m**3}
+!> \describepar{frac_Rub_ini , f_R , fraction of Rubisco, 0.4 -}
+!> describepar{Rub          , f_R\mathrm{Phy}_\mathrm{C}, trait x biomass}
+!> \describepar{frac_chl_ini , f_\theta\theta_C , Chl:C ratio, 0.034 mg-Chla/mmol-C}
+!> describepar{chl          , f_\theta\theta_C\mathrm{Phy}_\mathrm{C}, trait x biomass}
 real(rk)  :: nutN_initial ! Dissolved Inorganic Nitrogen DIN
 real(rk)  :: nutP_initial ! Dissolved Inorganic Phosphorus DIP
 real(rk)  :: nutS_initial ! Dissolved Inorganic Silicon Si
@@ -119,11 +138,56 @@ real(rk)  :: frac_Rub_ini ! fraction of Rubisco
 real(rk)  :: Rub  ! trait times biomass
 real(rk)  :: frac_chl_ini ! Chl:C ratio
 real(rk)  :: chl  ! trait times biomass
+!> describepar{P_max        , P_\mathrm{max}        , maximum potential photosynthetic rate, 12.0 d^{-1}}
+!> describepar{alpha        , \alpha        , specific light adsorption by chloroplasts, 0.12 m2 mol-C/(muE g-CHL)}
+!> describepar{sigma        , \sigma        , Q-dependency of Rubisco activity/chloroplast ratio, 0.0 }
+!> describepar{theta_LHC    , \theta_\mathrm{C}    , Chlorophylla-to-C ratio of LHC, 1.2 mgChl mmolC^{-1}}
+!> describepar{rel_chloropl_min ,  , chloroplast-C to phy-C ratio, 0.01 mol-C/mol-C}
+!> describepar{QN_phy_0     , Q_{\mathrm{N}0}     , subsistence N-quota, 0.045 mol-N/mol-C}
+!> describepar{QN_phy_max   , Q_\mathrm{N}^\mathrm{ref}   , maximum N-quota, 0.24 mol-N/mol-C}
+!> describepar{V_NC_max     , V_\mathrm{max,N}^0     , maximum N uptake rate, 0.3 mol-N/(mol-C d)}
+!> describepar{AffN         , A_\mathrm{N}^0         , N-Affinity, 0.1 m3/(mmol-C d)}
+!> describepar{zeta_CN      , \zeta_\mathrm{CN}      , respiratory costs of N-synthesis/NO3-reduction, 2.3 mol-C/mol-N}
+!> describepar{zstoich_PN   ,    , P-stoichiometry of active compounds(-> P costs), 3.8 mol-N/mol-P}
+!> describepar{exud_phy     ,      , phytoplankton exudation per production, 0.0 }
+!> describepar{QP_phy_0     , Q_{\mathrm{P}0}     , subsistence P-quota, 0.0 mol-P/mol-C}
+!> describepar{QP_phy_max   , Q_\mathrm{P}^\mathrm{ref}   , subsistence P-quota, 0.02 mol-P/mol-C}
+!> describepar{V_PC_max     , V_\mathrm{max,P}^0     , maximum P uptake rate, 0.03 mol-P/(mol-C d)}
+!> describepar{AffP         , A_\mathrm{P}^0         , P-Affinity, 0.07 m3/(mmol-C d)}
+!> describepar{QSi_phy_0    , Q_{\mathrm{Si}0}    , subsistence Si-quota, 0.0 mol-Si/mol-C}
+!> describepar{QSi_phy_max  , Q_\mathrm{Si}^\mathrm{ref}  , subsistence Si-quota, 0.1 mol-Si/mol-C}
+!> describepar{V_SiC_max    , V_\mathrm{max,Si}^0    , maximum Si-uptake rate, 0.02 mol-Si/(mol-C d)}
+!> describepar{AffSi        , A_\mathrm{Si}^0        , Si-Affinity, 0.05 m3/(mmol-C d)}
+!> describepar{syn_nut      , 1/h      , synchrony n_queue in nutrient quota limitation, 2.0 }
+!> describepar{adap_rub     , \delta_R     , adap_rub, 1.0 }
+!> describepar{adap_theta   , \delta_\theta   , adap_theta, 1.0 }
+!> describepar{tau_regV     , \Delta t_\mathrm{v}     , tau_regV, 99.0 }
+!> describepar{phi_agg      ,       , quadratic aggregation rate, 5E-4 m^6 mmolN^{-2} d^{-1}}
+!> describepar{vS_phy       ,        , sinking velocity for phytoplankton, 5E-2 m d^{-1}}
+!> describepar{vS_det       ,        , sinking velocity for detritus, 2. m d^{-1}}
+!> describepar{hydrol       ,        , hydrolysis rate, 0.03 d^{-1}}
+!> describepar{remin        ,         , pelagic remineralisation, 0.03 d^{-1}}
+!> describepar{Ae_all       ,        , Activation energy, 4500.0 ...}
+!> describepar{T_ref        ,         , reference temperature, 288.0 degC}
+!> describepar{NutOrder     ,      , element order of recursive scheme. lower digit: synchrony element   , 123.1 serial order number N:P:Si}
+!> describepar{const_NC_zoo ,  , zooplankton N:C ratio, 0.3 mol/mol}
+!> describepar{const_PC_zoo ,  , zooplankton P:C ratio, 0.025 mol/mol}
+!> describepar{g_max        ,         , maximum grazing rate, 1. per d}
+!> describepar{k_grazC      ,       , half saturation graing, 5.0 mmolN/m**3}
+!> describepar{yield_zoo    ,     , yield of herbivory, 0.35 }
+!> describepar{basal_resp_zoo ,  , basal respiration, 0.04 per d}
+!> describepar{mort_zoo     ,      , quadratic mortality, 0.035 m**3/mmolN.d}
+!> describepar{a_water      ,       , background attenuation coefficient, 0.01 1/m}
+!> describepar{a_spm        ,         , attenuation coefficient of SPM, 0.001 m**3/m.mmolC}
+!> describepar{a_chl        ,         , attenuation coefficient due to Chl absorption, 0.02 m**3/m.mgChl}
+!> describepar{frac_PAR     ,      , photosynthetically active fraction of light, 1.0 }
+!> describepar{small        ,         , lower limit for denominator in ratios; small_finite=sqrt(small), 1e-04 }
+!> describepar{dil          ,           , dilution of all concentrations except dissolved inorganics, 0.0 }
 !!------- Parameters from nml-list maecs_pars ------- 
 real(rk)  :: P_max        ! maximum potential photosynthetic rate
-real(rk)  :: alpha        ! alpha
+real(rk)  :: alpha        ! specific light adsorption by chloroplasts
 real(rk)  :: sigma        ! Q-dependency of Rubisco activity/chloroplast ratio
-real(rk)  :: theta_LHC    ! chlorophyll a-to-C ratio of LHC
+real(rk)  :: theta_LHC    ! Chlorophylla-to-C ratio of LHC
 real(rk)  :: rel_chloropl_min ! chloroplast-C to phy-C ratio
 real(rk)  :: QN_phy_0     ! subsistence N-quota
 real(rk)  :: QN_phy_max   ! maximum N-quota
@@ -140,7 +204,7 @@ real(rk)  :: QSi_phy_0    ! subsistence Si-quota
 real(rk)  :: QSi_phy_max  ! subsistence Si-quota
 real(rk)  :: V_SiC_max    ! maximum Si-uptake rate
 real(rk)  :: AffSi        ! Si-Affinity
-real(rk)  :: syn_nut      ! synchrony n_queue nutrient quota limitation
+real(rk)  :: syn_nut      ! synchrony n_queue in nutrient quota limitation
 real(rk)  :: adap_rub     ! adap_rub
 real(rk)  :: adap_theta   ! adap_theta
 real(rk)  :: tau_regV     ! tau_regV
@@ -151,7 +215,7 @@ real(rk)  :: hydrol       ! hydrolysis rate
 real(rk)  :: remin        ! pelagic remineralisation
 real(rk)  :: Ae_all       ! Activation energy
 real(rk)  :: T_ref        ! reference temperature
-real(rk)  :: NutOrder     ! element order of recursive scheme. default 123...   
+real(rk)  :: NutOrder     ! element order of recursive scheme. lower digit: synchrony element   
 !!------- Parameters from nml-list maecs_graz ------- 
 real(rk)  :: const_NC_zoo ! zooplankton N:C ratio
 real(rk)  :: const_PC_zoo ! zooplankton P:C ratio
@@ -222,38 +286,38 @@ domN_initial = 0.01_rk            ! mmol-N/m**3
 domP_initial = 0.001_rk           ! mmol-P/m**3
 frac_Rub_ini = 0.4_rk             ! -
 frac_chl_ini = 0.034_rk           ! mg-Chla/mmol-C
-P_max        = 15.0_rk            ! d^{-1}
+P_max        = 12.0_rk            ! d^{-1}
 alpha        = 0.12_rk            ! m2 mol-C/(muE g-CHL)
-sigma        = 1.0_rk             ! 
+sigma        = 0.0_rk             ! 
 theta_LHC    = 1.2_rk             ! mgChl mmolC^{-1}
-rel_chloropl_min = 0.03_rk            ! mol-C/mol-C
-QN_phy_0     = 0.04_rk            ! mol-N/mol-C
-QN_phy_max   = 0.2_rk             ! mol-N/mol-C
-V_NC_max     = 0.2_rk             ! mmol-N/(m3 d)
-AffN         = 0.1_rk             ! m3/(mmol-N d)
+rel_chloropl_min = 0.01_rk            ! mol-C/mol-C
+QN_phy_0     = 0.045_rk           ! mol-N/mol-C
+QN_phy_max   = 0.24_rk            ! mol-N/mol-C
+V_NC_max     = 0.3_rk             ! mol-N/(mol-C d)
+AffN         = 0.1_rk             ! m3/(mmol-C d)
 zeta_CN      = 2.3_rk             ! mol-C/mol-N
 zstoich_PN   = 3.8_rk             ! mol-N/mol-P
 exud_phy     = 0.0_rk             ! 
 QP_phy_0     = 0.0_rk             ! mol-P/mol-C
 QP_phy_max   = 0.02_rk            ! mol-P/mol-C
-V_PC_max     = 0.03_rk            ! mmol-P/(m3 d)
-AffP         = 0.07_rk            ! m3/(mmol-N d)
-QSi_phy_0    = 0.04_rk            ! mol-P/mol-C
-QSi_phy_max  = 0.2_rk             ! mol-P/mol-C
-V_SiC_max    = 0.2_rk             ! mmol-P/(m3 d)
-AffSi        = 0.1_rk             ! m3/(mmol-N d)
+V_PC_max     = 0.03_rk            ! mol-P/(mol-C d)
+AffP         = 0.07_rk            ! m3/(mmol-C d)
+QSi_phy_0    = 0.0_rk             ! mol-Si/mol-C
+QSi_phy_max  = 0.1_rk             ! mol-Si/mol-C
+V_SiC_max    = 0.02_rk            ! mol-Si/(mol-C d)
+AffSi        = 0.05_rk            ! m3/(mmol-C d)
 syn_nut      = 2.0_rk             ! 
 adap_rub     = 1.0_rk             ! 
 adap_theta   = 1.0_rk             ! 
 tau_regV     = 99.0_rk            ! 
-phi_agg      = 18E-4_rk           ! m^6 mmolN^{-2} d^{-1}
-vS_phy       = 40E-2_rk           ! m d^{-1}
+phi_agg      = 5E-4_rk            ! m^6 mmolN^{-2} d^{-1}
+vS_phy       = 5E-2_rk            ! m d^{-1}
 vS_det       = 2._rk              ! m d^{-1}
 hydrol       = 0.03_rk            ! d^{-1}
 remin        = 0.03_rk            ! d^{-1}
 Ae_all       = 4500.0_rk          ! ...
 T_ref        = 288.0_rk           ! degC
-NutOrder     = 21_rk              ! serial order number N:P:Si
+NutOrder     = 123.1_rk           ! serial order number N:P:Si
 const_NC_zoo = 0.3_rk             ! mol/mol
 const_PC_zoo = 0.025_rk           ! mol/mol
 g_max        = 1._rk              ! per d
