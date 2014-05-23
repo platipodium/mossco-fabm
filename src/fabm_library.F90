0389ad6bce60e3ac104bd772f8d2b66621223318
Onur Kerimoglu <onur.kerimoglu@hzg.de>
2014-05-23 19:43:13 +0200

    fabm_library: re-organized

--- src/fabm_library.F90
+++ src/fabm_library.F90
@@ -32,7 +32,7 @@
 !   use fabm_hzg_omexdia_p
    use fabm_iow_spm
 !   use fabm_iow_age
-!   use fabm_iow_ergom
+   use fabm_iow_ergom
 !   use fabm_msi_ergom1
    use fabm_hzg_maecs
    use fabm_hzg_jelly
@@ -98,16 +98,16 @@
 !         case ('examples_benthic_predator'); allocate(type_examples_benthic_predator::model)
 !         case ('examples_mean');             allocate(type_examples_mean::model)
          case ('gotm_npzd');                 allocate(type_gotm_npzd::model)
-         case ('gotm_fasham');               allocate(type_gotm_fasham::model)
-         case ('gotm_ergom');                allocate(type_gotm_ergom::model)
-         case ('metu_mnemiopsis');           allocate(type_metu_mnemiopsis::model)
-         case ('pml_carbonate');             allocate(type_pml_carbonate::model)
-         case ('klimacampus_phy_feedback');  allocate(type_klimacampus_phy_feedback::model)
-         case ('hzg_omexdia_p');             allocate(type_hzg_omexdia_p::model)
+!        case ('gotm_fasham');               allocate(type_gotm_fasham::model)
+!         case ('gotm_ergom');                allocate(type_gotm_ergom::model)
+!         case ('metu_mnemiopsis');           allocate(type_metu_mnemiopsis::model)
+!         case ('pml_carbonate');             allocate(type_pml_carbonate::model)
+!         case ('klimacampus_phy_feedback');  allocate(type_klimacampus_phy_feedback::model)
+!         case ('hzg_omexdia_p');             allocate(type_hzg_omexdia_p::model)
          case ('iow_spm');                   allocate(type_iow_spm::model)
 !         case ('iow_age');                   allocate(type_iow_age::model)
 !         case ('msi_ergom1');                allocate(type_msi_ergom1::model)
-!         case ('iow_ergom');                 allocate(type_iow_ergom::model)
+         case ('iow_ergom');                 allocate(type_iow_ergom::model)
          case ('hzg_maecs');                 allocate(type_hzg_maecs::model)
          case ('hzg_jelly');                 allocate(type_hzg_jelly::model)
          case ('hzg_n2pzdq');          allocate(type_hzg_n2pzdq::model)
