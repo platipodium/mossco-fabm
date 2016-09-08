fa4fcab88a4c8fd596583b3cb4aeb0d896b62fc1
Richard <richard.hofmeister@hzg.de>
2016-09-08 10:52:40 +0200

    dependencies: add test declarations

--- src/models/hzg/dependencies/dependencies.F90
+++ src/models/hzg/dependencies/dependencies.F90
@@ -14,6 +14,7 @@
 !
 ! !USES:
    use fabm_types
+   use fabm_standard_variables
 
    implicit none
 
@@ -33,6 +34,7 @@
    type,extends(type_base_model) :: type_hzg_dependencies
 !     Variable identifiers
       type (type_dependency_id)            :: id_temp,id_par
+      type (type_horizontal_dependency_id) :: id_o2flux, id_oduflux
       type (type_diagnostic_variable_id)   :: id_dtemp,id_dpar
 
       contains
@@ -79,6 +81,8 @@
    ! Register dependencies
    call self%register_dependency(self%id_temp,standard_variables%temperature)
    call self%register_dependency(self%id_par,standard_variables%downwelling_photosynthetic_radiative_flux)
+   call self%register_dependency(self%id_o2flux, 'dissolved_oxygen_upward_flux_at_soil_surface','mmolO2 m-2','dissolved_oxygen_upward_flux_at_soil_surface')
+   call self%register_dependency(self%id_oduflux, standard_variable=type_horizontal_standard_variable('dissolved_reduced_substances_upward_flux_at_soil_surface','mmolO2 m-2','dissolved_reduced_substances_upward_flux_at_soil_surface'))
 
    return
 
