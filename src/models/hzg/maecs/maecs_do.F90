aa3acd090a176e26085fca0183618ce3a03f3c1f
Kai Wirtz <kai.wirtz@hzg.de>
2016-11-22 17:22:58 +0100

    diel vert. migrat -> ZooMort

--- src/models/hzg/maecs/maecs_do.F90
+++ src/models/hzg/maecs/maecs_do.F90
@@ -283,9 +283,9 @@ if (self%GrazingOn) then
       _GET_GLOBAL_ (self%id_doy,doy) !day of year
       _GET_HORIZONTAL_(self%id_zmax, zmax)  ! max depth
 !       relmort=1.0d0 + self%zm_fa_delmax*sens%f_T2*0.5*(1-sin(2*(doy+75)*Pi/365.0))/(att+self%zm_fa_inf)
-       fa = 1.0_rk !/(1.0_rk+exp(2*(att-self%zm_fa_inf)))
+!       fa = 1.0_rk !/(1.0_rk+exp(2*(att-self%zm_fa_inf)))
        relmort=1.0d0 + self%zm_fa_delmax*sens%f_T2*0.25*(1-sin(2*(doy+75)*Pi/365.0))**2 
-       relmort = relmort*(0.5+0.5/(1+exp(0.333*(zmax-30.0))))
+       relmort = relmort*(self%zm_fa_inf+(1.0_rk-self%zm_fa_inf)/(1+exp(0.333*(zmax-35.0))))
      case (1)
        fa = 1.0_rk/(1.0_rk+exp(2*(att-self%zm_fa_inf)))
        relmort = 1.0_rk + self%zm_fa_delmax* fa
