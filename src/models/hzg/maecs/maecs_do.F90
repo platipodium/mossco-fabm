d5972be03c7704ea401066b5d1f7b8f35cb13bea
Kai Wirtz kai.wirtz@hzg.de <kai.wirtz@hzg.de>
2017-02-18 16:39:26 +0100

    adj Zoo mort in transit

--- src/models/hzg/maecs/maecs_do.F90
+++ src/models/hzg/maecs/maecs_do.F90
@@ -274,8 +274,8 @@ if (self%GrazingOn) then
       _GET_GLOBAL_ (self%id_doy,doy) !day of year
       _GET_HORIZONTAL_(self%id_zmax, zmax)  ! max depth
 
-       fa = self%zm_fa_inf +(1.0_rk-self%zm_fa_inf)/(1+exp(0.3*(zmax-20.0)))
-       relmort = fa + (fa)*self%zm_fa_delmax*sens%f_T2*0.25*(1-sin(2*(doy+45)*Pi/365.0))**2
+       fa = self%zm_fa_inf +(1.0_rk-self%zm_fa_inf)/(1+exp(0.25*(zmax-25.0)))
+       relmort = fa + fa*self%zm_fa_delmax*sens%f_T2*0.25*(1-sin(2*(doy+45)*Pi/365.0))**2
        ksat_graz = fa * self%k_grazC
      case (1)
        fa = 1.0_rk/(1.0_rk+exp(2*(att-self%zm_fa_inf)))
