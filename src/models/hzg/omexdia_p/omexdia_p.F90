07394ed75f7b743edec7d3fd454c90c6e6fc5bce
Markus Kreus <markus.kreus@baw.de>
2017-09-15 19:32:29 +0200

    hzg_omexdia_p: added comments (e.g. units) and minor code cosmetics

--- src/models/hzg/omexdia_p/omexdia_p.F90
+++ src/models/hzg/omexdia_p/omexdia_p.F90
@@ -79,32 +79,32 @@
 !
 ! !LOCAL VARIABLES:
 !!------- Initial values -------
-   real(rk)  :: ldetC_init    ! labile detritus carbon (fast decay)
-   real(rk)  :: sdetC_init    ! semilabile detritus carbon (slow decay)
-   real(rk)  :: detP_init     ! detritus phosphorus
-   real(rk)  :: po4_init      ! dissolved phosphate
-   real(rk)  :: no3_init      ! dissolved nitrate
-   real(rk)  :: nh3_init      ! dissolved ammonium
-   real(rk)  :: oxy_init      ! dissolved oxygen
-   real(rk)  :: odu_init      ! dissolved reduced substances
-!!------- Parameters -------
-   real(rk)  :: rFast         ! decay rate labile detritus (fast decay)
-   real(rk)  :: rSlow         ! decay rate semilabile detritus (slow decay)
-   real(rk)  :: NCrLdet       ! NC ratio labile detritus (fast decay)
-   real(rk)  :: NCrSdet       ! NC ratio semilabile detritus (slow decay)
-   real(rk)  :: PAds          ! Adsorption coeff phosphorus
-   real(rk)  :: PAdsODU       ! PO4-Fe dissolution threshold in terms of [FeS]/ODU
-   real(rk)  :: NH3Ads        ! Adsorption coeff ammonium
-   real(rk)  :: CprodMax=48.0 ! Max C-degrad rate for numeric stability
-   real(rk)  :: rnit          ! Max nitrification rate
-   real(rk)  :: ksO2nitri     ! half-sat O2 in nitrification
-   real(rk)  :: rODUox        ! Max rate oxidation of ODU
-   real(rk)  :: ksO2oduox     ! half-sat O2 in oxidation of ODU
-   real(rk)  :: ksO2oxic      ! half-sat O2 in oxic mineralis
-   real(rk)  :: ksNO3denit    ! half-sat NO3 in denitrif
-   real(rk)  :: kinO2denit    ! half-sat O2 inhib denitrif
-   real(rk)  :: kinNO3anox    ! half-sat NO3 inhib anoxic min
-   real(rk)  :: kinO2anox     ! half-sat O2 inhib anoxic min
+   real(rk)  :: ldetC_init    ! labile detritus carbon (fast decay)         (        mmolC  m-3 solid)
+   real(rk)  :: sdetC_init    ! semilabile detritus carbon (slow decay)     (        mmolC  m-3 solid)
+   real(rk)  :: detP_init     ! detritus phosphorus                         (        mmolO2 m-3 liquid)
+   real(rk)  :: po4_init      ! dissolved phosphate                         (        mmol   m-3 liquid)
+   real(rk)  :: no3_init      ! dissolved nitrate                           (        mmolN  m-3 liquid)
+   real(rk)  :: nh3_init      ! dissolved ammonium                          (        mmolN  m-3 liquid)
+   real(rk)  :: oxy_init      ! dissolved oxygen                            (        mmolP  m-3 solid)
+   real(rk)  :: odu_init      ! dissolved reduced substances                (        mmolP  m-3 liquid)
+!!------- Parameters -------  ! (reference Values refer to Soetaert (1996))
+   real(rk)  :: rFast         ! decay rate labile detritus (fast decay)     (        d-1)
+   real(rk)  :: rSlow         ! decay rate semilabile detritus (slow decay) (        d-1)
+   real(rk)  :: NCrLdet       ! N/C ratio labile detritus (fast decay)      (0.1509  molN molC-1)
+   real(rk)  :: NCrSdet       ! N/C ratio semilabile detritus (slow decay)  (0.1333  molN molC-1)
+   real(rk)  :: PAds          ! Adsorption coeff phosphorus                 (        -)
+   real(rk)  :: PAdsODU       ! PO4-Fe dissolution threshold in terms of [FeS]/ODU ( -)
+   real(rk)  :: NH3Ads        ! Adsorption coeff ammonium                   (1.3     -)
+   real(rk)  :: CprodMax=48.0 ! Max C-degrad rate for numeric stability     (        -)
+   real(rk)  :: rnit          ! Max nitrification rate                      (20.0    d-1)
+   real(rk)  :: ksO2nitri     ! half-sat for O2  limitation in nitrification                 (1.0  mmolO2 m-3)
+   real(rk)  :: rODUox        ! Max rate oxidation rate of ODU              (20.0    d-1)
+   real(rk)  :: ksO2oduox     ! half-sat for O2  limitation in oxidation of reduced nutriens (1.0  mmolO2 m-3)
+   real(rk)  :: ksO2oxic      ! half-sat for O2  limitation in oxic mineralization           (3.0  mmolO2 m-3)
+   real(rk)  :: ksNO3denit    ! half-sat for NO3 limitation in denitrification               (6.18 mmolN  m-3 := 30.0 mmolNO3 m-3)
+   real(rk)  :: kinO2denit    ! half-sat for O2  inhibition in denitrification               (10.0 mmolO2 m-3)
+   real(rk)  :: kinNO3anox    ! half-sat for NO3 inhibition in anoxic mineralization         (1.03 mmolN  m-3 := 5.0 mmolNO3 m-3)
+   real(rk)  :: kinO2anox     ! half-sat for O2  inhibition in anoxic mineralization         (5.0  mmolO2 m-3)
 
    namelist /hzg_omexdia_p/  &
           rFast, rSlow, NCrLdet, NCrSdet, PAds, PAdsODU, NH3Ads, &
@@ -177,7 +177,7 @@
    ! Register diagnostic variables
    call self%register_diagnostic_variable(self%id_adsp, 'adsP', 'mmolP/m**3',  &
          'phosphate adsorption', output=output_instantaneous)
-   call self%register_diagnostic_variable(self%id_denit, 'denit', 'mmol/m**3/d',  &
+   call self%register_diagnostic_variable(self%id_denit, 'denit', 'mmolN/m**3/d',  &
          'denitrification rate', output=output_instantaneous)
 
    ! Register dependencies
@@ -210,9 +210,12 @@
 !  Original author(s): Richard Hofmeister & Kai Wirtz
 !
 ! !LOCAL VARIABLES:
-   real(rk), parameter :: relaxO2=0.04_rk
-   real(rk), parameter :: T0 = 288.15_rk ! reference Temperature fixed to 15 degC
-   real(rk), parameter :: Q10b = 1.5_rk
+   real(rk), parameter :: gammaNO3 = 0.8_rk    ! molN  used per molC in denitrification     !RH 0.8-> ~104/106?
+   real(rk), parameter :: gammaO2  = 1.0_rk    ! molO2 used per molC in oxic mineralization !RH 1.0->150/106*OxicMin (if [oxy]=mmolO2/m**3)
+   real(rk), parameter :: gammaNH3 = 2.0_rk    ! molO2 needed to oxidize one molNH3 in nitrification
+   real(rk), parameter :: T0       = 288.15_rk ! reference Temperature fixed to 15 degC
+   real(rk), parameter :: Q10b     = 1.5_rk    ! q10 temperature coefficient (source?)
+   real(rk), parameter :: relaxO2  = 0.04_rk   ! (source?)
    real(rk) :: ldetC, sdetC, oxy, odu, no3, nh3, detP, po4
    real(rk) :: temp_celsius, temp_kelvin, f_T, E_a
    real(rk) :: radsP, Oxicminlim, Denitrilim, Anoxiclim, Rescale, rP
@@ -226,76 +229,79 @@
    _LOOP_BEGIN_
 
    ! Retrieve current (local) state variable values.
-   _GET_(self%id_temp, temp_celsius) ! sediment-water temperature
-   _GET_(self%id_ldetC, ldetC)       ! labile detritus C in mmolC/m**3
-   _GET_(self%id_sdetC, sdetC)       ! semilabile detritus C in mmolC/m**3
-   _GET_(self%id_detP, detP)         ! detritus P in mmolP/m**3
-   _GET_(self%id_oxy, oxy)           ! dissolved oxygen in mmolO2/m**3
-   _GET_(self%id_odu, odu)           ! dissolved reduced substances in mmolO2/m**3
-   _GET_(self%id_no3, no3)           ! dissolved nitrate in mmolN/m**3
-   _GET_(self%id_nh3, nh3)           ! dissolved ammonium in mmolN/m**3
-   _GET_(self%id_po4, po4)           ! dissolved phosphate in mmolP/m**3
+   _GET_(self%id_temp, temp_celsius) ! sediment-water temperature (degC)
+   _GET_(self%id_ldetC, ldetC)       ! labile detritus C          (mmolC/m**3)
+   _GET_(self%id_sdetC, sdetC)       ! semilabile detritus C      (mmolC/m**3)
+   _GET_(self%id_detP, detP)         ! detritus P                 (mmolP/m**3)
+   _GET_(self%id_oxy, oxy)           ! dissolved oxygen           (mmolO2/m**3)
+   _GET_(self%id_odu, odu)           ! dissolved oxygen demand units (mmolO2/m**3)
+   _GET_(self%id_no3, no3)           ! dissolved nitrate          (mmolN/m**3)
+   _GET_(self%id_nh3, nh3)           ! dissolved ammonium         (mmolN/m**3)
+   _GET_(self%id_po4, po4)           ! dissolved phosphate        (mmolP/m**3)
 
+   ! temperature dependency
    temp_kelvin = 273.15_rk + temp_celsius
    E_a = 0.1_rk*log(Q10b)*T0*(T0+10.0_rk)
-   f_T = 1.0_rk*exp(-E_a*(1.0_rk/temp_kelvin - 1.0_rk/T0))
+   f_T = 1.0_rk*exp(-E_a*(1.0_rk/temp_kelvin - 1.0_rk/T0)) ! temperature factor (-)
 
-   if (2*oxy < -self%kinO2anox) oxy = -self%kinO2anox/2
+   if (2*oxy < -self%kinO2anox) oxy = -self%kinO2anox/2    ! limit anoxic conditions
 
-   Oxicminlim = oxy/(oxy+self%ksO2oxic+relaxO2*(nh3+odu))                ! limitation terms
+   ! limitation terms
+   Oxicminlim = oxy/(oxy+self%ksO2oxic+relaxO2*(nh3+odu))                                ! (-)
+   Denitrilim = (1.0_rk-oxy/(oxy+self%kinO2denit)) * NO3/(no3+self%ksNO3denit)           ! (-)
+   Anoxiclim  = (1.0_rk-oxy/(oxy+self%kinO2anox)) * (1.0_rk-no3/(no3+self%kinNO3anox))   ! (-)
+   if(Oxicminlim - 1E-3 < -Denitrilim-Anoxiclim) Oxicminlim = 1E-3-Denitrilim-Anoxiclim  ! (-)
+   Rescale    = 1.0_rk/(Oxicminlim+Denitrilim+Anoxiclim)     !Soetaert eq 3.4
 
-   Denitrilim = (1.0_rk-oxy/(oxy+self%kinO2denit)) * NO3/(no3+self%ksNO3denit)
-   Anoxiclim  = (1.0_rk-oxy/(oxy+self%kinO2anox)) * (1.0_rk-no3/(no3+self%kinNO3anox))
-   if(Oxicminlim - 1E-3 < -Denitrilim-Anoxiclim) Oxicminlim = 1E-3-Denitrilim-Anoxiclim
-   Rescale    = 1.0_rk/(Oxicminlim+Denitrilim+Anoxiclim)
+   CprodF = f_T * self%rFast * ldetC   ! (mmolC m-3 d-1)
+   CprodS = f_T * self%rSlow * sdetC   ! (mmolC m-3 d-1)
 
-   CprodF = f_T * self%rFast * ldetC
-   CprodS = f_T * self%rSlow * sdetC
-
-! assume upper reactive surface area for POC hydrolysis
+! assume upper reactive surface area for POC hydrolysis (introduced by Kai Wirtz ?)
    if (CprodS > self%CprodMax) CprodS = self%CprodMax
    if (CprodF > self%CprodMax) CprodF = self%CprodMax
 
-   Cprod  = CprodF + CprodS
-   Nprod  = CprodF * self%NCrLdet + CprodS * self%NCrSdet
-
+   Cprod  = CprodF + CprodS                               ! (mmolC m-3 d-1)
+   Nprod  = CprodF * self%NCrLdet + CprodS * self%NCrSdet ! (mmolN m-3 d-1) !MK: how to implement N/C-ratio from (coupled) external model?
 
 ! PO4-adsorption ceases when critical capacity is reached
 ! [FeS] approximated by ODU
 ! TODO: temperature dependency
-   radsP  = self%PAds  * po4 * 1.0_rk/(1.0_rk+exp(-5.0_rk+(odu-oxy)/self%PAdsODU))
-   rP     = f_T * self%rFast * 2 ! (1.0_rk - Oxicminlim)
-   Pprod  = rP * detP
+   radsP  = self%PAds  * po4 * 1.0_rk/(1.0_rk+exp(-5.0_rk+(odu-oxy)/self%PAdsODU)) ! (?)
+   rP     = f_T * self%rFast * 2 ! (1.0_rk - Oxicminlim)  ! (d-1)
+   Pprod  = rP * detP                                     ! (mmolP m-3 d-1)
 
 ! Oxic mineralisation, denitrification, anoxic mineralisation
 ! then the mineralisation rates
-   OxicMin    = Cprod*Oxicminlim*Rescale        ! oxic mineralisation
-   Denitrific = Cprod*Denitrilim*Rescale        ! Denitrification
-   AnoxicMin  = Cprod*Anoxiclim *Rescale        ! anoxic mineralisation
+   OxicMin    = Cprod*Oxicminlim*Rescale  ! carbon oxidized by O2              (mmolC m-3 d-1) !Soetaert eq 3.1
+   Denitrific = Cprod*Denitrilim*Rescale  ! carbon oxidized by denitrification (mmolC m-3 d-1) !Soetaert eq 3.2
+   AnoxicMin  = Cprod*Anoxiclim *Rescale  ! carbon oxidized by other oxidants  (mmolC m-3 d-1) !Soetaert eq 3.3
 
-! reoxidation and ODU deposition
-   Nitri      = f_T * self%rnit   * nh3 * oxy/(oxy + self%ksO2nitri + relaxO2*(ldetC + odu))
-   OduOx      = f_T * self%rODUox * odu * oxy/(oxy + self%ksO2oduox + relaxO2*(nh3 + ldetC))
+! Ammonium (NH3) nitrified (mmolN m-3 d-1) !Soetaert eq 3.7
+   Nitri      = f_T * self%rnit   * nh3 * oxy/(oxy + self%ksO2nitri + relaxO2*(ldetC + odu)) !Soetaert eq 3.7, but where does the relax* term comes from?
+
+! Oxygen consumed in the oxidation of reduced substances (mmolO2 m-3 d-1) !Soetaert eq 3.6
+   OduOx      = f_T * self%rODUox * odu * oxy/(oxy + self%ksO2oduox + relaxO2*(nh3 + ldetC)) !Soetaert eq 3.6, but where does the relax* term comes from?
    if (OduOx > self%CprodMax) OduOx = self%CprodMax
 
+! ODU deposition
 !  pDepo      = min(1.0_rk,0.233_rk*(wDepo)**0.336_rk )
    pDepo      = 0.0_rk
-   OduDepo    = AnoxicMin*pDepo
+   OduDepo    = AnoxicMin*pDepo           ! ODU deposited as solids (in O2-equivalents) (mmolO2 m-3 d-1) !Soetaert eq 3.5
 
 #define _CONV_UNIT_ *one_pr_day
 ! reaction rates
-   _SET_ODE_(self%id_ldetC, -CprodF _CONV_UNIT_)
-   _SET_ODE_(self%id_sdetC, -CprodS _CONV_UNIT_)
-   _SET_ODE_(self%id_oxy,  (-OxicMin - 2.0_rk* Nitri - OduOx) _CONV_UNIT_) !RH 1.0->150/106*OxicMin (if [oxy]=mmolO2/m**3)
-   _SET_ODE_(self%id_no3,  (-0.8_rk*Denitrific + Nitri) _CONV_UNIT_)       !RH 0.8-> ~104/106?
-!   _SET_ODE_(self%id_nh3,  (Nprod - Nitri) / (1.0_rk + self%NH3Ads) _CONV_UNIT_)
-   _SET_ODE_(self%id_nh3,  (Nprod - Nitri) _CONV_UNIT_)
-   _SET_ODE_(self%id_odu,  (AnoxicMin - OduOx - OduDepo) _CONV_UNIT_)
-   _SET_ODE_(self%id_po4,  (Pprod - radsP) _CONV_UNIT_)
-   _SET_ODE_(self%id_detP, (radsP - Pprod - self%NH3Ads*detP) _CONV_UNIT_)
+   _SET_ODE_(self%id_ldetC, -CprodF                                        _CONV_UNIT_)  ! (mmolC  m-3 d-1)
+   _SET_ODE_(self%id_sdetC, -CprodS                                        _CONV_UNIT_)  ! (mmolC  m-3 d-1)
+   _SET_ODE_(self%id_oxy,  (-OxicMin*gammaO2     - Nitri*gammaNH3 - OduOx) _CONV_UNIT_)  ! (mmolO2 m-3 d-1)
+   _SET_ODE_(self%id_no3,  (-Denitrific*gammaNO3 + Nitri)                  _CONV_UNIT_)  ! (mmolN  m-3 d-1)
+!   _SET_ODE_(self%id_nh3,  (Nprod - Nitri) / (1.0_rk + self%NH3Ads)       _CONV_UNIT_)  ! (mmolN  m-3 d-1)
+   _SET_ODE_(self%id_nh3,  (Nprod - Nitri)                                 _CONV_UNIT_)  ! (mmolN  m-3 d-1)
+   _SET_ODE_(self%id_odu,  (AnoxicMin - OduOx - OduDepo)                   _CONV_UNIT_)  ! (mmolO2 m-3 d-1)
+   _SET_ODE_(self%id_po4,  (Pprod - radsP)                                 _CONV_UNIT_)  ! (mmolP  m-3 d-1)
+   _SET_ODE_(self%id_detP, (radsP - Pprod - self%NH3Ads*detP)              _CONV_UNIT_)  ! (mmolP  m-3 d-1)
 
    ! Export diagnostic variables
-   _SET_DIAGNOSTIC_(self%id_denit, 0.8_rk*Denitrific)
+   _SET_DIAGNOSTIC_(self%id_denit, Denitrific*gammaNO3)
    _SET_DIAGNOSTIC_(self%id_adsp, radsP)
 
    ! Leave spatial loops (if any)
